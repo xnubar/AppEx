0147ff4186cc6796a2b23a2516099f9a2e678a92
Nubar Khalidova <nkh.nubarkhalid@gmail.com>
2018-10-29 15:47:39 +0400

    MVVM light

--- AppEx/App.xaml.cs
+++ AppEx/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AppEx.View;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -13,5 +14,11 @@ namespace AppEx
     /// </summary>
     public partial class App : Application
     {
+        public App()
+        {
+            var viewModelLocator = new ViewModelLocator();
+            var app = new AppView();
+            app.DataContext = viewModelLocator.appViewModel;
+        }
     }
 }
--- AppEx/ViewModelLocator.cs
+++ AppEx/ViewModelLocator.cs
@@ -1,12 +1,20 @@
-﻿using System;
+﻿using AppEx.Navigation;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Navigation;
+using NavigationService = AppEx.Navigation.NavigationService;
 
 namespace AppEx
 {
-   public class ViewModelLocator
+    public class ViewModelLocator
     {
+        private INavigationService navigationService;
+        public ViewModelLocator()
+        {
+            navigationService = new NavigationService();
+        }
     }
 }
