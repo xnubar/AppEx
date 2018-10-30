994d0cdfd3d9694e3889796a5167b34164c73b1e
Nubar Khalidova <nkh.nubarkhalid@gmail.com>
2018-10-30 11:02:27 +0400

    Filter view/vm

---
+++ AppEx/Model/Person.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppEx.Model
+{
+    public class Person
+    {
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string Lastname { get; set; }
+        public string FatherName { get; set; }
+        public string BirthDate { get; set; }
+        public string FIN { get; set; }
+        public string Address { get; set; }
+        public string GivenDate { get; set; }
+        public string ExireDate { get; set; }
+        public string SerialNo { get; set; }
+        public string Gender { get; set; }
+
+        public int? CompanyId { get; set; }
+        public virtual Company Company { get; set; }
+    }
+}
--- AppEx/ViewModel/FilterViewModel.cs
+++ AppEx/ViewModel/FilterViewModel.cs
@@ -1,7 +1,9 @@
-﻿using AppEx.Navigation;
+﻿using AppEx.Model;
+using AppEx.Navigation;
 using GalaSoft.MvvmLight;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,5 +17,12 @@ namespace AppEx.ViewModel
         {
             this.navigation = navigation;
         }
+        private ObservableCollection<Person> people;
+        public ObservableCollection<Person> People
+        {
+            get { return people; }
+            set { Set(ref people,value); }
+        }
+
     }
 }
